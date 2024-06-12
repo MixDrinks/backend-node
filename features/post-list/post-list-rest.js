421dc824d3677153604efb488c96e81e065a7043
Volodymyr Stelmashchuk <volodymyrstelmashchuk@Volodymyrs-MacBook-Pro.local>
2024-06-12 19:03:42 +0300

    add paddination

--- features/post-list/post-list-rest.js
+++ features/post-list/post-list-rest.js
@@ -1,16 +1,23 @@
 const express = require('express');
-const { Blog } = require('../../database/schemas');
+const {Blog} = require('../../database/schemas');
 const router = express.Router();
 
 router.get('/api/blog/post-list', (req, res) => {
-  Blog.find({}, 'title slug image', (err, blogs) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send(err);
-    } else {
-      res.status(200).json(blogs);
-    }
-  });
+    const page = req.query.page || 1;
+    const pageSize = 12
+
+    Blog.find({}, 'title slug image')
+        .sort({published_at: -1})
+        .skip((page - 1) * pageSize)
+        .limit(pageSize)
+        .exec()
+        .then(blogs => {
+            res.status(200).json(blogs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send(err);
+        });
 });
 
 module.exports = router;
