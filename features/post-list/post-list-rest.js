9bc83b7b9ca1994ddbd51da406067936258d6faa
Volodymyr Stelmashchuk <volodymyrstelmashchuk@Volodymyrs-MacBook-Pro.local>
2024-06-12 19:11:30 +0300

    add page count

--- features/post-list/post-list-rest.js
+++ features/post-list/post-list-rest.js
@@ -2,22 +2,26 @@ const express = require('express');
 const {Blog} = require('../../database/schemas');
 const router = express.Router();
 
-router.get('/api/blog/post-list', (req, res) => {
+router.get('/api/blog/post-list', async (req, res) => {
     const page = req.query.page || 1;
     const pageSize = 12
 
-    Blog.find({}, 'title slug image')
+    const result = await Blog.find({}, 'title slug image')
         .sort({published_at: -1})
         .skip((page - 1) * pageSize)
         .limit(pageSize)
         .exec()
-        .then(blogs => {
-            res.status(200).json(blogs);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).send(err);
-        });
+
+    const pageCount = Math.ceil(await Blog.countDocuments() / pageSize);
+
+    if (!result) {
+        res.status(404).send('No blog found');
+    }
+
+    res.status(200).json({
+        posts: result,
+        pageCount: pageCount
+    });
 });
 
 module.exports = router;
