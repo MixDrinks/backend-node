e422e650ca577d079c0f27363fc95009314d07cb
Volodymyr Stelmashchuk <volodymyrstelmashchuk@Volodymyrs-MacBook-Pro.local>
2024-06-14 11:34:09 +0300

    Migrate to db url

--- database/db.js
+++ database/db.js
@@ -1,24 +1,17 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const dbUser = process.env.DB_USER;
-const dbPassword = encodeURIComponent(process.env.DB_PASSWORD);
-const dbHost = process.env.DB_HOST;
-const dbPort = process.env.DB_PORT;
-const dbName = process.env.DB_NAME;
-
-const uri = `mongodb://${dbUser}:${dbPassword}@${dbHost}:${dbPort}`;
+const uri = process.env.DB_URL
 
 mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  dbName: dbName,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('We are connected to MongoDB!');
+db.once('open', function () {
+    console.log('We are connected to MongoDB!');
 });
 
 module.exports = mongoose;
