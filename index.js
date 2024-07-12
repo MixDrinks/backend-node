7a5d4c5f5abd35e325cac94ee3ae144e6f1e9409
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2024-07-12 22:25:38 +0300

    update

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const express = require('express');
 const postListRoutes = require('./features/post-list/post-list-rest');
 const postDetailsRoutes = require('./features/post-list/post-details-rest');
 const cocktailDetails = require('./features/cocktail/rest');
+const service = require('./features/service');
 
 const app = express();
 const port = process.env.APP_PORT;
 
 app.use(express.json());
 
-app.use(postListRoutes, postDetailsRoutes, cocktailDetails);
+app.use(postListRoutes, postDetailsRoutes, cocktailDetails, service);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
