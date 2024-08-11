313245024ba1e92cf9baea6534f3dee013509676
Vova Stelmashchuk <vovochkastelmashcuk@gmail.com>
2024-08-11 09:19:06 +0000

    fixed cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
 const postListRoutes = require('./features/post-list/post-list-rest');
 const postDetailsRoutes = require('./features/post-list/post-details-rest');
 const cocktailDetails = require('./features/cocktail/rest');
@@ -8,6 +10,7 @@ const service = require('./features/service');
 const app = express();
 const port = process.env.APP_PORT;
 
+app.use(cors());
 app.use(express.json());
 
 app.use(postListRoutes, postDetailsRoutes, cocktailDetails, service);
