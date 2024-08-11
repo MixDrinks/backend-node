0f51c06113d79e16abf2445c2ad0498fea53b030
Vova Stelmashchuk <vovochkastelmashcuk@gmail.com>
2024-08-11 09:33:17 +0000

    fix cors

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ const service = require('./features/service');
 const app = express();
 const port = process.env.APP_PORT;
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: '*', // Equivalent to anyHost()
+  methods: '*', // Equivalent to allowing all methods
+  allowedHeaders: '*', // Equivalent to allowHeaders { true }
+  credentials: true, // Equivalent to allowCredentials = true
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+}));
 
 app.use(postListRoutes, postDetailsRoutes, cocktailDetails, service);
 
--- utils/image.js
+++ utils/image.js
@@ -7,10 +7,10 @@ const buildImages = (id, type) => {
   };
   const formats = ["webp", "jpg"];
   const sizes = [
-    {responseSize: "570", imageSize: "origin"},
-    {responseSize: "410", imageSize: "560"},
-    {responseSize: "330", imageSize: "400"},
-    {responseSize: "0", imageSize: "320"}
+    { responseSize: "570", imageSize: "origin" },
+    { responseSize: "410", imageSize: "560" },
+    { responseSize: "330", imageSize: "400" },
+    { responseSize: "0", imageSize: "320" }
   ];
 
   return formats.flatMap(format =>
